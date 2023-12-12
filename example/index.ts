6ae446bfc660d6af4fa6d4c8189d63a866804f22
Siong Ong <siong1987@gmail.com>
2023-12-13 01:37:10 +0800

    show how to submit transaction

--- example/index.ts
+++ example/index.ts
@@ -1,4 +1,7 @@
 import { createJupiterApiClient, IndexedRouteMapResponse } from "../src/index";
+import { Connection, Keypair, VersionedTransaction } from "@solana/web3.js";
+import { Wallet } from "@project-serum/anchor";
+import bs58 from "bs58";
 
 type RouteMap = Record<string, string[]>;
 
@@ -30,6 +33,14 @@ function inflateIndexedRouteMap(
 
 export async function main() {
   const jupiterQuoteApi = createJupiterApiClient();
+  const wallet = new Wallet(
+    Keypair.fromSecretKey(bs58.decode(process.env.PRIVATE_KEY || ""))
+  );
+
+  // make sure that you are using your own RPC endpoint
+  const connection = new Connection(
+    "https://neat-hidden-sanctuary.solana-mainnet.discover.quiknode.pro/2af5315d336f9ae920028bbb90a73b724dc1bbed/"
+  );
 
   // get quote
   const quote = await jupiterQuoteApi.quoteGet({
@@ -47,14 +58,30 @@ export async function main() {
   }
 
   // get serialized transaction
-  const swapResult = await jupiterQuoteApi.swapInstructionsPost({
+  const swapResult = await jupiterQuoteApi.swapPost({
     swapRequest: {
       quoteResponse: quote,
-      userPublicKey: "HAPdsaZFfQDG4bD8vzBbPCUawUWKSJxvhQ7TGg1BeAxZ",
+      userPublicKey: wallet.publicKey.toBase58(),
     },
   });
 
   console.dir(swapResult, { depth: null });
+
+  // submit transaction
+  const swapTransactionBuf = Buffer.from(swapResult.swapTransaction, "base64");
+  var transaction = VersionedTransaction.deserialize(swapTransactionBuf);
+  console.log(transaction);
+
+  // sign the transaction
+  transaction.sign([wallet.payer]);
+
+  const rawTransaction = transaction.serialize();
+  const txid = await connection.sendRawTransaction(rawTransaction, {
+    skipPreflight: true,
+    maxRetries: 2,
+  });
+  await connection.confirmTransaction(txid);
+  console.log(`https://solscan.io/tx/${txid}`);
 
   // get route map
   const result = await jupiterQuoteApi.indexedRouteMapGet();
