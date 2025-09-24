9816c8d1e9a6a81139e7850570c9cc43651c59e5
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-09-24 14:27:59 +0200

    Token API With Generated Spec

--- generated/models/index.ts
+++ generated/models/index.ts
@@ -1,11 +1,16 @@
 /* tslint:disable */
 /* eslint-disable */
 export * from './AccountMeta';
+export * from './ErrorResponse';
 export * from './IndexedRouteMapResponse';
 export * from './Instruction';
+export * from './MintInformation';
+export * from './MintInformationAudit';
+export * from './MintInformationFirstPool';
 export * from './PlatformFee';
 export * from './QuoteResponse';
 export * from './RoutePlanStep';
+export * from './SearchTokens400Response';
 export * from './SwapInfo';
 export * from './SwapInstructionsResponse';
 export * from './SwapMode';
@@ -13,3 +18,4 @@ export * from './SwapRequest';
 export * from './SwapRequestPrioritizationFeeLamports';
 export * from './SwapRequestPrioritizationFeeLamportsPriorityLevelWithMaxLamports';
 export * from './SwapResponse';
+export * from './SwapStats';
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,52 @@
+import { TokenApi } from "generated/apis";
 import { SwapApi } from "../generated/apis/SwapApi";
 import { ConfigurationParameters, Configuration } from "../generated/runtime";
 
+interface UrlConfig {
+  PUBLIC: string;
+  API_KEY: string;
+}
+
 // Define server URLs
-const PUBLIC_SERVER_URL = "https://lite-api.jup.ag/swap/v1";
-const API_KEY_SERVER_URL = "https://api.jup.ag/swap/v1";
+const serverUrls: Record<string, UrlConfig> = {
+  swap: {
+    PUBLIC: "https://lite-api.jup.ag/swap/v1",
+    API_KEY: "https://api.jup.ag/swap/v1",
+  },
+  token: {
+    PUBLIC: "https://lite-api.jup.ag/tokens/v2",
+    API_KEY: "https://api.jup.ag/tokens/v2",
+  },
+}
 
 /**
  * Creates a Jupiter API client with optional API key support
  * @param config Configuration parameters
- * @returns SwapApi instance
+ * @returns Jupiter API instance partitioned by category
  */
 export const createJupiterApiClient = (config?: ConfigurationParameters) => {
+  return {
+    swap: new SwapApi(urlConfig(serverUrls.swap, config)),
+    token: new TokenApi(urlConfig(serverUrls.token, config))
+  }
+};
+
+/**
+ * Attaches Server Urls to optional ConfigurationParameters
+ * @param urls Server Urls (i.e. API Routes)
+ * @param config Configuration parameters
+ * @returns Api Configuration with Server Urls
+ */
+const urlConfig = (urls: UrlConfig, config?: ConfigurationParameters): Configuration => {
   // Determine which server URL to use based on whether an API key is provided
   const hasApiKey = config?.apiKey !== undefined;
-  
   // Create a new configuration with the appropriate base path
   const configWithServer: ConfigurationParameters = {
     ...config,
-    basePath: hasApiKey ? API_KEY_SERVER_URL : PUBLIC_SERVER_URL,
+    basePath: hasApiKey ? urls.API_KEY : urls.PUBLIC,
     headers: hasApiKey ? { 'x-api-key': config?.apiKey as string } : undefined
   };
-  
-  return new SwapApi(new Configuration(configWithServer));
-};
+  return new Configuration(configWithServer);
+}
 
 export * from "../generated";
